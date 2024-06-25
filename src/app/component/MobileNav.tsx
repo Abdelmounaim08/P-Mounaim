e473c4af057eac2e1a908ccba7dbf4cd4641fe08
Elabiade <abdomel2002@email.com>
2024-06-25 10:42:21 +0100

    navMobile edit

--- src/app/component/MobileNav.tsx
+++ src/app/component/MobileNav.tsx
@@ -11,7 +11,7 @@ interface Props{
   const navAnimation =nav ? 'translate-x-0' : 'translate-x-[-100%]';
   return (
     <div className={`fixed ${navAnimation} z-[100000000]  transform transition-all duration-300 top-0 left-0 bottom-0 right-0 bg-[#09101a]`}>
-    <div className={`w-[100vw] h-[100vw] flex flex-col items-center justify-center`}>
+    <div className={`w-[100vw] h-[100vw] flex flex-col items-center justify-center pt-[10rem]`}>
               <div className="nav-link-mobile"><Link href="#home" onClick={closeNav}>HOME</Link></div>
               <div className="nav-link-mobile"><Link onClick={closeNav} href="#service">SERVICES</Link></div>
               <div className="nav-link-mobile"><Link onClick={closeNav} href="#ABOUT">ABOUT</Link></div>
