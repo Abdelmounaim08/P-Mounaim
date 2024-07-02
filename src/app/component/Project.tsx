6f42f51f36f00bcb894fb8314b97408fb9afa95e
Elabiade <abdomel2002@email.com>
2024-07-02 12:14:04 +0100

    link

--- src/app/component/Project.tsx
+++ src/app/component/Project.tsx
@@ -34,19 +34,19 @@ function Project() {
     <div>
     <h3 className='text-white rounded-md mx uppercase text-center italic text-[1rem]'>private school </h3>
     <div className='w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem]'>
-    <a href="https://github.com/Abdelmounaim08/Aljabr_Ecole" className='transform cursor-pointer  hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
+    <a href="https://ecolealjabr.com/" className='transform cursor-pointer  hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
          
     <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
       <Image src='/image/aljaber.png' alt='portfolio' layout='fill' className='object-contain'/> 
       
     </div></a>
-    <a href="https://github.com/Abdelmounaim08/Aljabr_Ecole" className='transform cursor-pointer  hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
+    <a href="https://ecolealjabr.com/" className='transform cursor-pointer  hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
     
      <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
         <Image src='/image/aljaberEcole.png' alt='portfolio' layout='fill' className='object-contain'/>
     </div>
     </a>
-    <a href="https://github.com/Abdelmounaim08/Aljabr_Ecole" className='transform cursor-pointer  hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
+    <a href="https://ecolealjabr.com/" className='transform cursor-pointer  hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
     
     <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
         <Image src='/image/aljaberDSh.png' alt='portfolio' layout='fill' className='object-contain'/>
